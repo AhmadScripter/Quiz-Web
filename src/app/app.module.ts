4c4d1638ca4e1a93f72b2e7496346ab072077a34
Ahmad Jutt <ahmadjut989@gmail.com>
2024-07-01 16:51:13 +0500

    completed the basic layout of quiz web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
+import { ResultComponent } from './pages/result/result.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    QuizComponent,
+    ResultComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
